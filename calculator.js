176f6fc72c8a0a9e29dd509fbb5af1ae610f647b
Michael Oluokun <105324113+Minor-Thefirst@users.noreply.github.com>
2022-06-23 22:19:50 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,20 @@
+let first = parseInt(prompt("Enter your first number"));
+let operator = prompt("Enter operator you wish to use from the list below: +, -, *, /");
+let second = parseInt(prompt("Enter your second number"));
+
+if (operator === "+") {
+    let addition = first + second;
+    alert(addition);
+} else if(operator ==="-"){
+    let subtraction = first - second;
+    alert(subtraction);
+} else if(operator ==="*"){
+    let multiplication = first * second;
+    alert(multiplication);
+} else if(operator ==="/"){
+    let division = first / second;
+    alert(division);
+} else (
+    alert("Wrong Input"));
+
+    
